230606e5598149ed66bfc25d7699163fbcee4af2
Seiji Takahashi <s_takahashi@dreamarts.co.jp>
2016-09-05 17:22:52 +0900

    mod

--- src/main/java/jp/co/dreamarts/chiwawa/Application.java
+++ src/main/java/jp/co/dreamarts/chiwawa/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-
+/**
+ *
+ */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude=HibernateJpaAutoConfiguration.class)
 public class Application {
